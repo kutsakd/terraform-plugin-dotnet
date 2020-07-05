603c4220ea4b44b4b1e0d37209eb031bd2e19d19
Denys Kutsak <67856698+kutsakd@users.noreply.github.com>
2020-07-05 20:59:15 +0300

    Added client certificate verification

--- src/NETCore.Terraform.SDK/Plugin.cs
+++ src/NETCore.Terraform.SDK/Plugin.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Https;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
 using System.IO;
 using System.IO.Compression;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 
 namespace NETCore.Terraform.SDK
 {
@@ -16,10 +22,28 @@ namespace NETCore.Terraform.SDK
         /// Initializes a new instance of the <see cref="HostBuilder"/> class with pre-configured defaults.
         /// </summary>
         /// <returns>The initialized <see cref="IHostBuilder"/>.</returns>
-        public static IHostBuilder CreateDefaultBuilder()
-        {
-            var builder = new HostBuilder();
+        public static IHostBuilder CreateDefaultBuilder() => CreateDefaultBuilder(new PluginOptions());
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HostBuilder"/> class with pre-configured defaults.
+        /// </summary>
+        /// <param name="options">Options for the current plugin and gRPC server.</param>
+        /// <returns>The initialized <see cref="IHostBuilder"/>.</returns>
+        public static IHostBuilder CreateDefaultBuilder(PluginOptions options)
+        {
+            if (string.IsNullOrEmpty(options.MagicCookieKey) || string.IsNullOrEmpty(options.MagicCookieValue))
+            {
+                throw new InvalidOperationException($"Misconfigured {nameof(PluginOptions)} given to serve this plugin: " +
+                    "no magic cookie key or value was set. Please notify the plugin author and report this as a bug.");
+            }
+
+            if (Environment.GetEnvironmentVariable(options.MagicCookieKey) != options.MagicCookieValue)
+            {
+                throw new InvalidOperationException("This binary is a plugin. These are not meant to be executed directly. " +
+                    "Please execute the program that consumes these plugins, which will load any plugins automatically.");
+            }
+
+            var builder = new HostBuilder();
             builder.UseContentRoot(Directory.GetCurrentDirectory());
             builder.ConfigureHostConfiguration(configuration =>
             {
@@ -42,12 +66,46 @@ namespace NETCore.Terraform.SDK
                 });
             });
 
+            var certificateContent = Environment.GetEnvironmentVariable("PLUGIN_CLIENT_CERT");
+            var certificate = ParseClientCertificate(certificateContent);
+            if (certificate != null)
+            {
+                builder.ConfigureWebHost(hostBuilder => hostBuilder.ConfigureKestrel(options =>
+                {
+                    options.ConfigureHttpsDefaults(connectionOptions =>
+                    {
+                        connectionOptions.SslProtocols = SslProtocols.Tls12;
+                        connectionOptions.ClientCertificateMode = ClientCertificateMode.RequireCertificate;
+                        connectionOptions.ClientCertificateValidation = (certificate, chain, _) =>
+                        {
+                            return certificate.Thumbprint == certificate.Thumbprint;
+                        };
+                    });
+                }));
+            }
+
+            Console.WriteLine("1|5|tcp|127.0.0.1:5001|grpc|");
             return builder.UseDefaultServiceProvider((context, options) =>
             {
                 var isDevelopment = context.HostingEnvironment.IsDevelopment();
                 options.ValidateScopes = isDevelopment;
                 options.ValidateOnBuild = isDevelopment;
             });
+        }
+
+        private static X509Certificate2 ParseClientCertificate(string certificateContent)
+        {
+            if (!string.IsNullOrWhiteSpace(certificateContent))
+            {
+                var certificateBlocks = certificateContent.Split('-', StringSplitOptions.RemoveEmptyEntries);
+                if (certificateBlocks.Length > 1)
+                {
+                    var certificateBytes = Convert.FromBase64String(certificateBlocks[1]);
+                    return new X509Certificate2(certificateBytes);
+                }
+            }
+
+            return null;
         }
     }
 }
---
+++ src/NETCore.Terraform.SDK/PluginOptions.cs
@@ -0,0 +1,27 @@
+﻿namespace NETCore.Terraform.SDK
+{
+    /// <summary>
+    /// Describes the options used to configure clients and servers.
+    /// </summary>
+    public class PluginOptions
+    {
+        /// <summary>
+        /// Gets the protocol version assumed for legacy clients that don't specify
+        /// a particular version during their handshake. This is the version used
+        /// when Terraform 0.10 and 0.11 launch plugins that were built with support
+        /// for both versions 4 and 5, and must stay unchanged at 4 until we intentionally
+        /// build plugins that are not compatible with 0.10 and 0.11.
+        /// </summary>
+        public const int DefaultProtocolVersion = 4;
+
+        public string MagicCookieKey => "TF_PLUGIN_MAGIC_COOKIE";
+        public string MagicCookieValue => "d602bf8f470bc67ca7faa0386276bbdd4330efaf76d1a219cb4d6991ca9872b2";
+
+        /// <summary>
+        /// Gets the version that must match between TF core and TF plugins.
+        /// This should be bumped whenever a change happens in one or the other
+        /// that makes it so that they can't safely communicate.
+        /// </summary>
+        public int ProtocolVersion => DefaultProtocolVersion;
+    }
+}
