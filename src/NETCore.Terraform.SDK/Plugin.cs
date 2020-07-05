77fef0d2550b2ab7733bb209e79e5b5b8a117763
Denys Kutsak <67856698+kutsakd@users.noreply.github.com>
2020-07-05 17:46:00 +0300

    Added existed gRPC protocol

---
+++ src/NETCore.Terraform.SDK/Plugin.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.IO.Compression;
+
+namespace NETCore.Terraform.SDK
+{
+    /// <summary>
+    /// Provides convenience methods for creating instances of <see cref="IHostBuilder"/> with pre-configured defaults.
+    /// </summary>
+    public static class Plugin
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HostBuilder"/> class with pre-configured defaults.
+        /// </summary>
+        /// <returns>The initialized <see cref="IHostBuilder"/>.</returns>
+        public static IHostBuilder CreateDefaultBuilder()
+        {
+            var builder = new HostBuilder();
+
+            builder.UseContentRoot(Directory.GetCurrentDirectory());
+            builder.ConfigureHostConfiguration(configuration =>
+            {
+                configuration.AddEnvironmentVariables(prefix: "TF_");
+            });
+
+            builder.ConfigureLogging((hostingContext, logging) =>
+            {
+                logging.AddDebug();
+                logging.AddEventSourceLogger();
+                logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+            });
+
+            builder.ConfigureServices((context, services) =>
+            {
+                services.AddGrpc(options =>
+                {
+                    options.EnableDetailedErrors = context.HostingEnvironment.IsDevelopment();
+                    options.ResponseCompressionLevel = CompressionLevel.Fastest;
+                });
+            });
+
+            return builder.UseDefaultServiceProvider((context, options) =>
+            {
+                var isDevelopment = context.HostingEnvironment.IsDevelopment();
+                options.ValidateScopes = isDevelopment;
+                options.ValidateOnBuild = isDevelopment;
+            });
+        }
+    }
+}
---
+++ src/NETCore.Terraform.SDK/ProvisionerService.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Core;
+using System.Threading.Tasks;
+using Tfplugin5;
+
+namespace NETCore.Terraform.SDK
+{
+    public class ProvisionerService : Provisioner.ProvisionerBase
+    {
+        public override Task<GetProvisionerSchema.Types.Response> GetSchema(
+            GetProvisionerSchema.Types.Request request, ServerCallContext context)
+        {
+            return base.GetSchema(request, context);
+        }
+
+        public override Task ProvisionResource(ProvisionResource.Types.Request request,
+            IServerStreamWriter<ProvisionResource.Types.Response> responseStream, ServerCallContext context)
+        {
+            return base.ProvisionResource(request, responseStream, context);
+        }
+
+        public override Task<Stop.Types.Response> Stop(Stop.Types.Request request, ServerCallContext context)
+        {
+            return base.Stop(request, context);
+        }
+
+        public override Task<ValidateProvisionerConfig.Types.Response> ValidateProvisionerConfig(
+            ValidateProvisionerConfig.Types.Request request, ServerCallContext context)
+        {
+            return base.ValidateProvisionerConfig(request, context);
+        }
+    }
+}
